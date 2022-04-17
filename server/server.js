d757bdab13787637c0ff787c55b5439686d562f8
Rakeeb <hossainrakeeb7233@gmail.com>
2022-04-17 15:14:31 -0400

    Added First router

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const db = require("./models");
 
+// Routers
+const flightRouter = require("./routes/Flights")
 
-app.listen(3001, () => console.log("Server running on port 3001"));
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => console.log("Server running on port 3001"));
+})
